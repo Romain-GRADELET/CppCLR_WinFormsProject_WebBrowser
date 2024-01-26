101810c3a3c2fcca8ff24d14f370971945b42344
Romain-GRADELET <romain.gradelet73@outlook.fr>
2024-01-26 10:05:04 +0100

    Interface et supression des erreur de script

--- Form1.h
+++ Form1.h
@@ -1,4 +1,8 @@
 #pragma once
+#include <iostream>
+#include <cstdlib>
+#include <string>
+
 
 namespace CppCLRWinFormsProject {
 
@@ -34,6 +38,18 @@ namespace CppCLRWinFormsProject {
 				delete components;
 			}
 		}
+	private: System::Windows::Forms::MenuStrip^ menuStrip1;
+	private: System::Windows::Forms::ToolStripMenuItem^ fichierToolStripMenuItem;
+	private: System::Windows::Forms::ToolStripMenuItem^ nouveauToolStripMenuItem;
+	private: System::Windows::Forms::ToolStripMenuItem^ quitterToolStripMenuItem;
+	private: System::Windows::Forms::ToolStripMenuItem^ aProposToolStripMenuItem;
+	private: System::Windows::Forms::ToolStripMenuItem^ toolStripMenuItem1;
+	private: System::Windows::Forms::TextBox^ textBox1;
+	private: System::Windows::Forms::Button^ button1;
+	private: System::Windows::Forms::WebBrowser^ webBrowser1;
+	private: System::Windows::Forms::Button^ button2;
+	private: System::Windows::Forms::Button^ button3;
+	protected:
 
 	private:
 		/// <summary>
@@ -48,12 +64,154 @@ namespace CppCLRWinFormsProject {
 		/// </summary>
 		void InitializeComponent(void)
 		{
-			this->components = gcnew System::ComponentModel::Container();
-			this->Size = System::Drawing::Size(300,300);
-			this->Text = L"Form1";
-			this->Padding = System::Windows::Forms::Padding(0);
+			this->menuStrip1 = (gcnew System::Windows::Forms::MenuStrip());
+			this->fichierToolStripMenuItem = (gcnew System::Windows::Forms::ToolStripMenuItem());
+			this->nouveauToolStripMenuItem = (gcnew System::Windows::Forms::ToolStripMenuItem());
+			this->quitterToolStripMenuItem = (gcnew System::Windows::Forms::ToolStripMenuItem());
+			this->aProposToolStripMenuItem = (gcnew System::Windows::Forms::ToolStripMenuItem());
+			this->toolStripMenuItem1 = (gcnew System::Windows::Forms::ToolStripMenuItem());
+			this->textBox1 = (gcnew System::Windows::Forms::TextBox());
+			this->button1 = (gcnew System::Windows::Forms::Button());
+			this->webBrowser1 = (gcnew System::Windows::Forms::WebBrowser());
+			this->button2 = (gcnew System::Windows::Forms::Button());
+			this->button3 = (gcnew System::Windows::Forms::Button());
+			this->menuStrip1->SuspendLayout();
+			this->SuspendLayout();
+			// 
+			// menuStrip1
+			// 
+			this->menuStrip1->Items->AddRange(gcnew cli::array< System::Windows::Forms::ToolStripItem^  >(3) {
+				this->fichierToolStripMenuItem,
+					this->aProposToolStripMenuItem, this->toolStripMenuItem1
+			});
+			this->menuStrip1->Location = System::Drawing::Point(0, 0);
+			this->menuStrip1->Name = L"menuStrip1";
+			this->menuStrip1->Size = System::Drawing::Size(1136, 24);
+			this->menuStrip1->TabIndex = 0;
+			this->menuStrip1->Text = L"menuStrip1";
+			// 
+			// fichierToolStripMenuItem
+			// 
+			this->fichierToolStripMenuItem->DropDownItems->AddRange(gcnew cli::array< System::Windows::Forms::ToolStripItem^  >(2) {
+				this->nouveauToolStripMenuItem,
+					this->quitterToolStripMenuItem
+			});
+			this->fichierToolStripMenuItem->Name = L"fichierToolStripMenuItem";
+			this->fichierToolStripMenuItem->Size = System::Drawing::Size(54, 20);
+			this->fichierToolStripMenuItem->Text = L"Fichier";
+			// 
+			// nouveauToolStripMenuItem
+			// 
+			this->nouveauToolStripMenuItem->Name = L"nouveauToolStripMenuItem";
+			this->nouveauToolStripMenuItem->Size = System::Drawing::Size(122, 22);
+			this->nouveauToolStripMenuItem->Text = L"Nouveau";
+			// 
+			// quitterToolStripMenuItem
+			// 
+			this->quitterToolStripMenuItem->Name = L"quitterToolStripMenuItem";
+			this->quitterToolStripMenuItem->Size = System::Drawing::Size(122, 22);
+			this->quitterToolStripMenuItem->Text = L"Quitter";
+			this->quitterToolStripMenuItem->Click += gcnew System::EventHandler(this, &Form1::quitterToolStripMenuItem_Click);
+			// 
+			// aProposToolStripMenuItem
+			// 
+			this->aProposToolStripMenuItem->Name = L"aProposToolStripMenuItem";
+			this->aProposToolStripMenuItem->Size = System::Drawing::Size(67, 20);
+			this->aProposToolStripMenuItem->Text = L"A propos";
+			this->aProposToolStripMenuItem->Click += gcnew System::EventHandler(this, &Form1::aProposToolStripMenuItem_Click);
+			// 
+			// toolStripMenuItem1
+			// 
+			this->toolStripMenuItem1->Name = L"toolStripMenuItem1";
+			this->toolStripMenuItem1->Size = System::Drawing::Size(24, 20);
+			this->toolStripMenuItem1->Text = L"\?";
+			this->toolStripMenuItem1->Click += gcnew System::EventHandler(this, &Form1::toolStripMenuItem1_Click);
+			// 
+			// textBox1
+			// 
+			this->textBox1->Location = System::Drawing::Point(12, 34);
+			this->textBox1->Name = L"textBox1";
+			this->textBox1->Size = System::Drawing::Size(857, 20);
+			this->textBox1->TabIndex = 1;
+			// 
+			// button1
+			// 
+			this->button1->Location = System::Drawing::Point(875, 32);
+			this->button1->Name = L"button1";
+			this->button1->Size = System::Drawing::Size(107, 23);
+			this->button1->TabIndex = 2;
+			this->button1->Text = L"Chercher";
+			this->button1->UseVisualStyleBackColor = true;
+			this->button1->Click += gcnew System::EventHandler(this, &Form1::button1_Click);
+			// 
+			// webBrowser1
+			// 
+			this->webBrowser1->Location = System::Drawing::Point(12, 60);
+			this->webBrowser1->MinimumSize = System::Drawing::Size(20, 20);
+			this->webBrowser1->Name = L"webBrowser1";
+			this->webBrowser1->ScriptErrorsSuppressed = true;
+			this->webBrowser1->Size = System::Drawing::Size(1112, 490);
+			this->webBrowser1->TabIndex = 3;
+			// 
+			// button2
+			// 
+			this->button2->Location = System::Drawing::Point(988, 32);
+			this->button2->Name = L"button2";
+			this->button2->Size = System::Drawing::Size(58, 23);
+			this->button2->TabIndex = 4;
+			this->button2->Text = L"<";
+			this->button2->UseVisualStyleBackColor = true;
+			// 
+			// button3
+			// 
+			this->button3->Location = System::Drawing::Point(1052, 32);
+			this->button3->Name = L"button3";
+			this->button3->Size = System::Drawing::Size(58, 23);
+			this->button3->TabIndex = 4;
+			this->button3->Text = L">";
+			this->button3->UseVisualStyleBackColor = true;
+			// 
+			// Form1
+			// 
+			this->AutoScaleDimensions = System::Drawing::SizeF(6, 13);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
+			this->ClientSize = System::Drawing::Size(1136, 562);
+			this->Controls->Add(this->button3);
+			this->Controls->Add(this->button2);
+			this->Controls->Add(this->webBrowser1);
+			this->Controls->Add(this->button1);
+			this->Controls->Add(this->textBox1);
+			this->Controls->Add(this->menuStrip1);
+			this->MainMenuStrip = this->menuStrip1;
+			this->Name = L"Form1";
+			this->Text = L"WebBrowser";
+			this->Load += gcnew System::EventHandler(this, &Form1::Form1_Load);
+			this->menuStrip1->ResumeLayout(false);
+			this->menuStrip1->PerformLayout();
+			this->ResumeLayout(false);
+			this->PerformLayout();
+
 		}
 #pragma endregion
-	};
+	private: System::Void aProposToolStripMenuItem_Click(System::Object^ sender, System::EventArgs^ e) {
+		System::Windows::Forms::MessageBox::Show("Développé avec moulte joie par Romain GRADELET", "A propos");
+	}
+
+	private: System::Void toolStripMenuItem1_Click(System::Object^ sender, System::EventArgs^ e) {
+		// Permet d'ouvrir une page internet ou un fichier pdf par exemple
+		System::Diagnostics::Process::Start("http://google.com");
+	}
+
+	// Quitter l'application via le menu
+	private: System::Void quitterToolStripMenuItem_Click(System::Object^ sender, System::EventArgs^ e) {
+		// Permet de quitter l'application 
+		System::Windows::Forms::Application::Exit();
+	}
+
+	// Bouton de recherche
+	private: System::Void button1_Click(System::Object^ sender, System::EventArgs^ e) {
+		this->webBrowser1->Navigate(this->textBox1->Text);
+	}
+
+};
 }
